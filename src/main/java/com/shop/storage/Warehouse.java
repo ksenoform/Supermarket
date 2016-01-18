1b33d81abd385872b84c9e3146334140185854e1
Robert <rszczygielski@luxoft.com>
2016-01-18 13:28:42 +0100

    in warehouse can now be sorage products with amount, add test methods

--- src/main/java/com/shop/storage/Warehouse.java
+++ src/main/java/com/shop/storage/Warehouse.java
@@ -3,13 +3,20 @@ package com.shop.storage;
 import com.shop.wares.Product;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by RSzczygielski on 17.01.16.
  */
 public interface Warehouse {
-    public List<Product> getAllProduct();
+    public Map<Product, Integer> getAllProduct();
     public Product getProductByID(String id);
     public Product getProductByName(String name);
-    public void addProduct(Product product);
+    public void addProductToWarehouse(Product product, Integer amount);
+    public void addProductToWarehouse(Product product, String amount);
+    public void addProductToWarehouse(Product product);
+    public void subtractProductFromWarehouse(Product product, Integer amount);
+    public void subtractProductFromWarehouse(Product product, String amount);
+    public void subtractProductFromWarehouse(Product product);
+
 }
