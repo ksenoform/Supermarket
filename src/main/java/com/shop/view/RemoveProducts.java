2d3dd9727848eb147743a077bf0ebf6d0ca36ccf
Robert <rszczygielski@luxoft.com>
2016-01-19 12:44:19 +0100

    Add new menu where user can remowe items from stock, correct remove method in WarehouseImpl

---
+++ src/main/java/com/shop/view/RemoveProducts.java
@@ -0,0 +1,40 @@
+package com.shop.view;
+
+import com.shop.storage.Warehouse;
+import com.shop.support.ConsoleReader;
+import com.shop.wares.Product;
+import com.shop.wares.ProductBuilderImpl;
+
+/**
+ * Created by RSzczygielski on 2016-01-19.
+ */
+public class RemoveProducts implements Commands {
+    Warehouse warehouse;
+
+    public RemoveProducts(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    @Override
+    public void execute() {
+        Product product = prepareProductWithOnlyIdToSubtract();
+        String amount = getAmountValue();
+
+        warehouse.subtractProductFromWarehouse(product, amount);
+    }
+
+    private String getAmountValue() {
+        System.out.println("How many products remove from stock: ");
+        String amount = ConsoleReader.readData();
+
+        return amount;
+    }
+
+    private Product prepareProductWithOnlyIdToSubtract() {
+        System.out.println(warehouse);
+        System.out.println("With product remove from stock (id): ");
+        String idFromUse = ConsoleReader.readData();
+
+        return warehouse.getProductByID(idFromUse);
+    }
+}
