8fafb9bae02ffad4593bc1c41d650414adbbb5ed
Ksenoform <ksenoform@gmail.com>
2016-02-04 15:56:00 +0100

    Add Interfate to managed the methods to writte and read from database, now all product add to the database willby displayed on the website prepated for this

--- src/main/java/com/shop/support/DatabaseConnector.java
+++ src/main/java/com/shop/support/DatabaseConnector.java
@@ -1,21 +1,13 @@
 package com.shop.support;
 
 import com.shop.wares.Product;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 /**
- * Created by RSzczygielski on 03.02.16.
+ * Created by RSzczygielski on 04.02.16.
  */
-@Repository
-@Transactional
-public class DatabaseConnector {
-    @Autowired
-    SessionFactory sessionFactory;
-
-    public void writeToDatabase(Product product) {
-        sessionFactory.getCurrentSession().save(product);
-    }
+public interface DatabaseConnector {
+    public void writeToDatabase(Product product);
+    public Map<Product, Integer> readFromDatabase();
 }
---
+++ src/main/java/com/shop/support/DatabaseConnectorImpl.java
@@ -0,0 +1,59 @@
+package com.shop.support;
+
+import com.shop.wares.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by RSzczygielski on 03.02.16.
+ */
+@Repository
+@Transactional
+public class DatabaseConnectorImpl implements DatabaseConnector{
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Override
+    public void writeToDatabase(Product product) {
+        sessionFactory.getCurrentSession().save(product);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Map<Product, Integer> readFromDatabase() {
+        Session session = sessionFactory.getCurrentSession();
+        List<Product> allProduct = session.createQuery("from ProductImpl").list();
+
+        Map<Product, Integer> result = addAmountOfProduct(allProduct);
+
+        return result;
+    }
+
+    private Map<Product, Integer> addAmountOfProduct(List<Product> allProduct) {
+        if (allProduct.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        Map<Product, Integer> toReturnWithAmount = new HashMap<Product, Integer>();
+
+        for (Product product : allProduct) {
+            Integer amount = getAmountFromDataBase(product);
+            toReturnWithAmount.put(product, amount);
+        }
+
+        return toReturnWithAmount;
+    }
+
+    private Integer getAmountFromDataBase(Product product) {
+        return 1;
+    }
+
+}
